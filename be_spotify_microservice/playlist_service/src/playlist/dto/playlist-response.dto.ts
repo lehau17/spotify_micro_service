b02c1c9aab903a3e34318a471708892f12145e8c
lehau17 <121428266+lehau17@users.noreply.github.com>
2025-01-16 16:59:10 +0700

    fixing type in playlistService

--- be_spotify_microservice/playlist_service/src/playlist/dto/playlist-response.dto.ts
+++ be_spotify_microservice/playlist_service/src/playlist/dto/playlist-response.dto.ts
@@ -1,4 +1,13 @@
 import { playlists } from '@prisma/client';
 import { SongDto } from './create-playlist.dto';
 
-export type PlaylistResponse = Omit<playlists, 'songs'> & { songs: SongDto[] };
+export type PlaylistResponse = {
+  id: number;
+  user_id: number;
+  image_path: string;
+  playlist_name: string;
+  description: string;
+  songs: SongDto[];
+  created_at: Date;
+  updated_at: Date;
+};
